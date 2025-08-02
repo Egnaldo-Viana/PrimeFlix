013aa98a865851985d82dff7039ec6368d0d2a86
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-08-01 23:52:20 -0300

    criacao da pagina detalhe do filme

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
 
 const Filme = () => {
+  const { id } = useParams();
+  const [filme, setFilme] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: '5bc4565984f5d84e202811fea28f3b64',
+            language: 'pt-BR',
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log('filme não encontrado');
+        });
+    }
+    loadFilme();
+
+    return () => {
+      console.log('componente foi desmontado');
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Página de filmes</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} /10</strong>
     </div>
   );
 };
