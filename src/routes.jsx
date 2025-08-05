4ca01439303e886eec9a50745d4fb8bde120638b
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-08-05 20:29:24 -0300

    Criando pagina favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+import Favoritos from './pages/Favoritos';
+
 import Erro from './pages/Erro';
+
 import Header from './components/Header';
 
 const RoutesApp = () => {
@@ -13,6 +16,7 @@ const RoutesApp = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
 
         <Route path="*" element={<Erro />} />
       </Routes>
