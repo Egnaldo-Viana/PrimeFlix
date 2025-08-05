08b94e8e0b9cf5ff5727b32078bd537d5b8f352a
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-08-05 20:51:32 -0300

    excluindo filme salvo na pagina favoritos

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -6,13 +6,33 @@ const Favoritos = () => {
   const [filmes, setFilmes] = React.useState([]);
 
   React.useEffect(() => {
+    // Busca a lista de filmes salva no localStorage com a chave '@primeflix'
     const minhaLista = localStorage.getItem('@primeflix');
+
+    // Converte o JSON para array e atualiza o estado 'filmes'
+    // Se não existir nada no localStorage, será definido como []
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
+
+  function excluirFilme(id) {
+    // Cria um novo array filtrando os filmes que não possuem o id passado
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
+    setFilmes(filtroFilmes);
+
+    // Atualiza o localStorage com a nova lista de filmes sem o filme excluído
+    localStorage.setItem('@primeflix', JSON.stringify(filtroFilmes));
+  }
 
   return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+
+      {filmes.length === 0 && (
+        <span> Você não possui nenhum filme salvo :( </span>
+      )}
+
       <ul>
         {filmes.map((filme) => {
           return (
@@ -20,7 +40,7 @@ const Favoritos = () => {
               <span>{filme.title}</span>
               <div>
                 <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(filme.id)}>Excluir</button>
               </div>
             </li>
           );
